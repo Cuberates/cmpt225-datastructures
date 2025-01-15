3a838b8ce26b251c09ccb4c1bdca3e3b9f26b955
Jack Nguyen <79352883+Cuberates@users.noreply.github.com>
2025-01-15 14:01:37 -0800

    need bug fixes

---
+++ assignment1/AList.h
@@ -0,0 +1,84 @@
+#ifndef ALIST_H
+#define ALIST_H
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+template <typename Object>
+class AList { 
+	public: 
+	AList() {
+		theCapacity = 12; 
+		objects = new Object[theCapacity];
+		theSize = 0; 
+		left = 0; // array index of location just before the left-most list element
+		right = 1; // array index just past the right-most list element
+	}
+	~AList() { delete [] objects; }
+	
+	bool empty() const { return size() == 0; }
+	int size() const { return theSize; }
+	void clear() { 
+		delete [] objects; 
+		objects = new Object[theCapacity];
+		theSize = 0; 
+		left = 0; 
+		right = 1; 
+	}
+
+	// Operations
+	void addLeft(const Object x) { 
+		theSize++; 
+		objects[left] = x; 
+		left = left - 1; 
+		if(left < 0) left = theCapacity - 1; 
+	}
+
+	void addRight(const Object x) { 
+		theSize++; 	
+		objects[right] = x; 
+		right = (right + 1) % theCapacity; 
+	}
+
+	Object removeRight() { 
+		Object rightElement = objects[(right - 1 + theCapacity) % theCapacity];
+		theSize--; 
+		right--; 
+		if(right < 0) right = theCapacity - 1;
+		return rightElement;  
+	}
+
+	Object removeLeft() { 
+		Object leftElement = objects[(left + 1) % theCapacity];
+		theSize--; 
+		left = (left + 1) % theCapacity; 
+		return leftElement;
+	}
+
+	void display() { 
+		cout << "The current queue: " << "\n";
+		int l = left; 
+		for(int i = 0; i < theSize; i++) {
+			l = (l + 1) % theCapacity; 
+			cout << objects[l] << " ";
+		}
+		cout << "\n";
+	}
+
+	void ddisplay() { 
+		cout << "Left: " << left << "\n";
+		cout << "Right: " << right << "\n";
+		cout << "Size: " << theSize << "\n";
+		display(); 
+	}
+
+	private:
+	int theSize;
+	int left; 
+	int right; 
+	int theCapacity; 
+	Object * objects;
+};
+
+#endif
